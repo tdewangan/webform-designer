e5ae57f6a519f1196f50453c77028e59dc8ea428
Tribhuwan Dewangan <dewangan.tribhuwan@gmail.com>
2023-10-03 08:02:58 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'zul-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'webform-designer';
+}
